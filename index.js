f3c1e400f27c72d0d4dca3c13fded53e76981eed
Yuguang (YG) Zhang <37890050+viztopia@users.noreply.github.com>
2024-10-16 22:18:24 -0400

    update lowdb server code

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 //Data array
-let defaultData = [
-  {
-    name: "Me",
-    message: "This is my first y message"
-  },
-  {
-    name: "You",
-    message: "Hello hello!"
-  }
-];
+// let defaultData = [
+//   {
+//     name: "Me",
+//     message: "This is my first y message"
+//   },
+//   {
+//     name: "You",
+//     message: "Hello hello!"
+//   }
+// ];
+
+//load lowdb module
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
+
+const defaultData = { myMessages: [] };
+const adapter = new JSONFile('db.json');
+const db = new Low(adapter, defaultData);
+
+// db.data = { ygmessages: [] };
 
 //Set up the server
-let express = require('express');
+// let express = require('express');
+import express from 'express';
 let app = express();
 
 //Serve static files from a public folder
 app.use(express.static('public'));
+app.use(express.json());
 
 //Set port variable to listen for requests
 let port = 3000;
@@ -24,4 +36,36 @@ app.listen(port, () => {
 });
 
 /*ROUTES */
+app.get('/messages', (request, response) => {
+  //Send data as an object
+  // response.json(defaultData);
 
+  // db.read --> get the latest value from db.data
+  db.read()
+    .then(() => {
+      //save the messages to an object
+      let theData = {messages : db.data.myMessages};
+      //send the messages to the client
+      response.json(theData);
+    });
+
+});
+
+app.post('/newMessage', (request, response) => {
+  // console.log(request.body);
+
+  let newMessage = request.body;
+  newMessage.time = Date();
+
+  // defaultData.push(newMessage);
+  db.data.myMessages.push(newMessage)
+
+  // write the latest db.data to the db.json file
+  db.write()
+    .then(() => {
+      //send message back to the client
+      response.json({ 'msg': 'Success' });
+    });
+
+  
+});
--- public/app.js
+++ public/app.js
@@ -2,4 +2,72 @@ window.addEventListener('load', () => {
 
   console.log("Client side js is loaded!");
 
+  fetch('/messages')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+
+      let theData = data.messages;
+
+      //Select for element on the page
+      let feed = document.getElementById('feed');
+      //Loop through data and append to the page
+      for (let i = 0; i < theData.length; i++) {
+        let currentName = theData[i].name;
+        let currentMessage = theData[i].message;
+
+        let currentEl = document.createElement('p');
+        currentEl.innerHTML = currentName + " - " + currentMessage;
+
+        feed.appendChild(currentEl);
+      }
+
+    })
+    .catch(error => {
+      console.log(error)
+    });
+});
+
+
+//Create an event listener to collect and POST data
+let msgButton = document.getElementById('msg-submit');
+msgButton.addEventListener('click', () => {
+  console.log("Button was clicked!");
+
+  let nameInput = document.getElementById('name-input');
+  let currentName = nameInput.value;
+
+  let msgInput = document.getElementById('msg-input')
+  let currentMessage = msgInput.value;
+
+  let messageObj = {
+    name: currentName,
+    message: currentMessage
+  };
+  console.log(messageObj);
+
+  let messageObjJSON = JSON.stringify(messageObj);
+
+  fetch('/newMessage', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: messageObjJSON
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Did this work?");
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+  let feed = document.getElementById('feed');
+  let currentEl = document.createElement('p');
+  currentEl.innerHTML = currentName + " - " + currentMessage;
+  feed.appendChild(currentEl);
+
+  nameInput.value = ''
+  msgInput.value = '';
+
 });
